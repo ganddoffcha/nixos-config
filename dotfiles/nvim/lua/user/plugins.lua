a3a7cde5bc731aea252d5962ecd73845ccf1d77d
gc <gc@zephyrus>
2026-07-14 21:14:09 +0800

    vimtex: quickfix_mode 0→1

--- dotfiles/nvim/lua/user/plugins.lua
+++ dotfiles/nvim/lua/user/plugins.lua
@@ -106,7 +106,7 @@ return {
       vim.g.vimtex_view_method = "zathura"
       vim.g.vimtex_compiler_method = "latexmk"
       vim.g.tex_flavor = "latex"
-      vim.g.vimtex_quickfix_mode = 0
+      vim.g.vimtex_quickfix_mode = 1
       vim.g.tex_conceal = "abdmg"
     end,
   },
